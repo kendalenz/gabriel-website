572233a06f98b0b888091039c0d7877b87a3dffa
Kendal Enz <kendal.enz@gmail.com>
2023-08-17 19:54:31 -0400

    fixed issue with images not showing up on cloudflare

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -1,3 +1,5 @@
+import archer from '@/assets/archer.png';
+
 const Home = () => {
 
   return (
@@ -6,7 +8,7 @@ const Home = () => {
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         <div className="md:order-1">
-          <img src="src/assets/archer.png" alt="Image" id="headshot" className="mx-auto md:mx-0"/>
+          <img src={archer} alt="Image" id="headshot" className="mx-auto md:mx-0"/>
         </div>
         <div className="md:order-2">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod augue ut imperdiet mollis. Integer eu lacinia quam, id scelerisque eros. In mollis, magna ut varius efficitur, quam mi posuere mauris, eget malesuada risus nulla molestie libero. Vivamus varius, libero nec condimentum commodo, est leo consectetur magna, eget mattis massa dolor vitae odio. Etiam nibh ex, convallis scelerisque ipsum vel, aliquet lacinia magna. In tincidunt iaculis mi ac varius. Nam eleifend dui ex, ut dictum purus sodales ut. Nam ornare mattis nisi nec sagittis. Praesent tempor maximus nunc eget placerat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque auctor mi vitae vehicula varius. Donec feugiat risus finibus lorem ornare, sit amet tincidunt tortor cursus. Nam vitae lobortis erat, vel porta mauris. Nam magna nunc, sodales id maximus at, euismod ut nisl. Maecenas ac est faucibus, dictum metus vel, molestie dui. Nunc eu aliquam metus, in placerat lorem.
