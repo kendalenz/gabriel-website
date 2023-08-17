cc6f2fa9c230b90469ed1043b796adf79bf914ac
Kendal Enz <kendal.enz@gmail.com>
2023-08-17 19:44:20 -0400

    testing cloudflare images

--- src/scenes/art/index.tsx
+++ src/scenes/art/index.tsx
@@ -1,3 +1,6 @@
+import art2 from '@/assets/art2.jpg';
+import art1 from '@/assets/art1.jpg';
+
 const Art = () => {
 
     return (
@@ -5,14 +8,14 @@ const Art = () => {
       <div className="">
       <div className="flex flex-wrap -mx-4">
         <div className="relative w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 my-8 mx-8">
-          <img src="src/assets/art2.jpg" alt="Photo 1" className="w-full h-auto rounded-lg" />
+          <img src={art2} alt="Photo 1" className="w-full h-auto rounded-lg" />
           <div className="absolute inset-0 flex items-center justify-center opacity-0 transition duration-300 bg-black bg-opacity-50 hover:opacity-100">
             <p className="text-white text-center">Description for Photo 1</p>
           </div>
         </div>
 
         <div className="relative w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 my-8 mx-8">
-          <img src="src/assets/art1.jpg" alt="Photo 2" className="w-full h-auto rounded-lg" />
+          <img src={art1} alt="Photo 2" className="w-full h-auto rounded-lg" />
           <div className="absolute inset-0 flex items-center justify-center opacity-0 transition duration-300 bg-black bg-opacity-50 hover:opacity-100">
             <p className="text-white text-center">Description for Photo 2</p>
           </div>
