105d82b86fb148ad600412f4a0c3c3cf06742abd
Kendal Enz <kendal.enz@gmail.com>
2023-08-29 16:30:07 -0400

    added real art

--- src/scenes/art/index.tsx
+++ src/scenes/art/index.tsx
@@ -1,5 +1,6 @@
-import art2 from '@/assets/art2.jpg';
-import art1 from '@/assets/art1.jpg';
+import AbstractExpressionism1 from '@/assets/AbstractExpressionism1.jpg';
+import AbstractExpressionism2 from '@/assets/AbstractExpresssionism2.jpg';
+import AbstractExpressionism3 from '@/assets/AbstractExpressionism3.jpg';
 import {motion} from 'framer-motion';
 
 const Art = () => {
@@ -15,6 +16,8 @@ const Art = () => {
 
   return (
     <div className='flex flex-wrap sm:mx-8 md:mx-20 my-24'>
+      <h1 className='text-2xl mx-8'>Abstract Expressionism</h1>
+
       <motion.div 
         className='md:flex items-start gap-0 mt-5'
         initial='hidden'
@@ -22,21 +25,30 @@ const Art = () => {
         viewport={{once: true, amount: 0.5}}
         variants={container}
       >
-        <div className='relative w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 my-8 mx-8 z-0'>
-          <div className='relative z-0'>
-            <img src={art2} alt='Photo 2' className='w-full h-auto rounded-lg z-0'/>
+         <div className='relative w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 my-8 mx-8'>
+          <div className='relative z-10'>
+            <img src={AbstractExpressionism1} alt='Untitled Abstract Expressionism 1' className='w-full h-auto rounded-lg'/>
             <div className='absolute inset-0 flex flex-col items-center justify-center opacity-0 transition duration-300 bg-black bg-opacity-50 hover:opacity-100 rounded-lg'>
-              <h4 className='text-white text-center'>Title</h4>
-              <p className='text-white text-center'>Description</p>
+              <h4 className='text-white text-center'>Untitled 1</h4>
+              {/* <p className='text-white text-center'>Description</p> */}
             </div>
           </div>
         </div>
-        <div className='relative w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 my-8 mx-8'>
-          <div className='relative z-10'>
-            <img src={art1} alt='Photo 1' className='w-full h-auto rounded-lg'/>
+        <div className='relative w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 my-8 mx-8 z-0'>
+          <div className='relative z-0'>
+            <img src={AbstractExpressionism2} alt='Untitled Abstract Expressionism 2' className='w-full h-auto rounded-lg z-0'/>
             <div className='absolute inset-0 flex flex-col items-center justify-center opacity-0 transition duration-300 bg-black bg-opacity-50 hover:opacity-100 rounded-lg'>
-              <h4 className='text-white text-center'>Title</h4>
-              <p className='text-white text-center'>Description</p>
+              <h4 className='text-white text-center'>Untitled 2</h4>
+              {/* <p className='text-white text-center'>Neo-abstract Idealism</p> */}
+            </div>
+          </div>
+        </div>
+        <div className='relative w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 my-8 mx-8 z-0'>
+          <div className='relative z-0'>
+            <img src={AbstractExpressionism3} alt='Untitled Abstract Expressionism 3' className='w-full h-auto rounded-lg z-0'/>
+            <div className='absolute inset-0 flex flex-col items-center justify-center opacity-0 transition duration-300 bg-black bg-opacity-50 hover:opacity-100 rounded-lg'>
+              <h4 className='text-white text-center'>Untitled 3</h4>
+              {/* <p className='text-white text-center'>Neo-abstract Idealism</p> */}
             </div>
           </div>
         </div>
