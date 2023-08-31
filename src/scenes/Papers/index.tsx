6a5c1c89f4654234c15afd34c73c380857586072
Kendal Enz <kendal.enz@gmail.com>
2023-08-31 12:12:19 -0400

    added all papers

--- src/scenes/Papers/index.tsx
+++ src/scenes/Papers/index.tsx
@@ -1,14 +1,38 @@
-import React from 'react';
-
 const DownloadPDF = () => {
-    const pdfUrl1 = '../src/assets/Rewriting-Group-Products-with-Transversals.pdf'; 
-    const pdfFileName = 'Rewriting-Group-Products-with-Transversals.pdf'; 
+  const pdfUrl3 = '../src/assets/Rewriting-Group-Products-with-Transversals.pdf'; 
+  const pdfFileName3 = 'Rewriting-Group-Products-with-Transversals.pdf'; 
   
+  const pdfUrl2 = '../src/assets/Preservation-of-Trees-by-Semidirect-Products.pdf';
+  const pdfFileName2 = 'Preservation-of-Trees-by-Semidirect-Products.pdf'
+    
+  const pdfUrl4 = '../src/assets/Using-Decision-Problems-in-Public-Key-Cryptography.pdf';
+  const pdfFileName4 = 'Using-Decision-Problems-in-Public-Key-Cryptography.pdf';
+
+  const pdfUrl1 = '../src/assets/Combinatorial-Group-Theory-and-Public-Key-Cryptography.pdf';
+  const pdfFileName1 = 'Combinatorial-Group-Theory-and-Public-Key-Cryptography.pdf';
+
     return (
       <div>
-        <a href={pdfUrl1} download={pdfFileName} className='underline'>
-        "Rewriting Group Products with Transversals"
-        </a>
+        <div className='mb-2'>
+          <a href={pdfUrl1} download={pdfFileName1} className='underline'>
+            "Combinatorial Group Theory and Public Key Cryptography"
+          </a>
+        </div>
+        <div className='mb-2'>
+          <a href={pdfUrl2} download={pdfFileName2} className='underline'>
+            "Preservation of Trees by Semidirect Products"
+          </a>
+        </div>
+        <div className='mb-2'>
+          <a href={pdfUrl3} download={pdfFileName3} className='underline'>
+            "Rewriting Group Products with Transversals"
+          </a>
+        </div>
+        <div>
+          <a href={pdfUrl4} download={pdfFileName4} className='underline'>
+            "Using Decision Problems in Public Key Cryptography"
+          </a>
+        </div>
       </div>
     );
   };
